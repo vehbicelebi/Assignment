f7bfdd3e96b8c42035082b08b9b29633c16a7e59
Celebi <vehbi.celebi@accenture.com>
2024-11-14 09:00:40 +0100

    first commit

---
+++ src/main/java/com/assignment/stableSoftware/StableSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.stableSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StableSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StableSoftwareApplication.class, args);
+	}
+
+}
